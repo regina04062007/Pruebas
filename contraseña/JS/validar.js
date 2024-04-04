dd65fb373d6f771f0c467cec38ea944014301844
regina04062007 <rherreram2200@alumno.ipn.mx>
2024-04-03 19:44:07 -0600

    avance6

---
+++ contraseña/JS/validar.js
@@ -0,0 +1,18 @@
+function validarFormulario() {
+    var codigo = document.getElementById('codigo').value;
+    var password = document.getElementById('password').value;
+    
+
+    if (codigo.length !== 6) {
+        alert('El código de verificación no es el correcto');
+        return false;
+    }
+
+    if (password.length !== 8) {
+        alert('La contraseña debe tener exactamente 8 caracteres');
+        return false;
+    }
+
+
+    return true; 
+}

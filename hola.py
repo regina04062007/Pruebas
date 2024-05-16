a2d3735012faae110b952cfcb1b3bf51c3b26fa6
regina04062007 <rherreram2200@alumno.ipn.mx>
2024-05-16 04:39:56 -0600

    avance11

---
+++ hola.py
@@ -0,0 +1,64 @@
+
+
+numero1 = float(input("Ingrese un numero"))
+numero2 = float(input("Ingrese un numero"))
+
+suma = numero1 + numero2
+
+print("La suma es: ", suma)
+
+import tkinter as tk
+def saludar():
+    label_xd.config(text = "Elías el pendejooooooooooooooooooooooooooooooooooooooooooooooooooooo00000000oooooooo")
+
+ventana = tk.TK()
+
+label_xd = tk.label_xd(ventana, text="")
+label_xd.pack(pady=10)
+
+boton_saludar = tk.button(ventana, text="Saludar", command=saludar)
+boton_saludar.pack()
+
+ventana.mainloop()
+
+
+numero1 = float(input("Ingrese un numero"))
+numero2 = float(input("Ingrese un numero"))
+
+suma = numero1 + numero2
+
+print("La suma es: ", suma)
+
+import tkinter as tk
+def saludar():
+    label_xd.config(text = "No lo se")
+
+ventana = tk.TK()
+
+label_xd = tk.label_xd(ventana, text="")
+label_xd.pack(pady=10)
+
+boton_saludar = tk.button(ventana, text="Saludar", command=saludar)
+boton_saludar.pack()
+
+ventana.mainloop()
+numero1 = float(input("Ingrese un numero"))
+numero2 = float(input("Ingrese un numero"))
+
+suma = numero1 + numero2
+
+print("La suma es: ", suma)
+
+import tkinter as tk
+def saludar():
+    label_xd.config(text = "No lo se")
+
+ventana = tk.TK()
+
+label_xd = tk.label_xd(ventana, text="")
+label_xd.pack(pady=10)
+
+boton_saludar = tk.button(ventana, text="Saludar", command=saludar)
+boton_saludar.pack()
+
+ventana.mainloop()
